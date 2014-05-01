d73937c7ee1f2f7e52ecbd3762f91c4f06d8a83d
Luiz Armesto <luiz.armesto@gmail.com>
2014-05-01 11:55:46 -0700

    adding update task

---
+++ scripts/update.sh
@@ -0,0 +1,27 @@
+git clone https://github.com/it3s/meppit-map.git meppit-map-tmp
+
+echo -n "Copying images... "
+cp -r meppit-map-tmp/dist/images vendor/assets/
+echo "done."
+
+echo -n "Copying javascript... "
+cp meppit-map-tmp/dist/meppit-map.full.js vendor/assets/javascripts/meppit-map.js
+echo "done."
+
+echo -n "Copying stylesheets... "
+cp meppit-map-tmp/dist/meppit-map.full.css vendor/assets/stylesheets/meppit-map.css.erb
+echo "done."
+
+echo -n "Fixing asset path... "
+sed -i -e "s/\(L.Icon.Default.imagePath = [(]path [?] path [+] '[/]' : ''[)]\) [+] 'images';/\1;/g" vendor/assets/javascripts/meppit-map.js
+sed -i -e "s/url[(]'\{0,1\}images[/]\(.*.png\)'\{0,1\}[)]/url(<%= asset_path '\1' %>)/g" vendor/assets/stylesheets/meppit-map.css.erb
+echo "done."
+
+echo -n "Updating version number... "
+VERSION=`sed -n -e 's/ \{0,\}"version": "\(.*\)", \{0,\}/\1/p' meppit-map-tmp/package.json`
+sed -i -e "s/VERSION = \".*\"/VERSION = \"${VERSION}\"/g" lib/meppit/map/rails/version.rb
+echo "done."
+
+echo -n "Cleaning up... "
+rm -rf meppit-map-tmp
+echo "done."
